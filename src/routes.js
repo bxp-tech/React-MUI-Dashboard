da8d7a334a42a35100066042a8acc356f6ee2514
Pablo Lozano <juanpa92008@gmail.com>
2021-06-02 18:10:33 -0500

    Update Login & NotFound

--- src/routes.js
+++ src/routes.js
@@ -5,14 +5,16 @@ import MainLayout from 'src/layouts/MainLayout';
 // import CustomerListView from 'src/views/customer/CustomerListView';
 // import AccountView from 'src/views/account/AccountView';
 // import DashboardView from 'src/views/reports/DashboardView';
-import LoginView from 'src/views/auth/LoginView';
-import NotFoundView from 'src/views/errors/NotFoundView';
+// import LoginView from 'src/views/auth/LoginView';
+// import NotFoundView from 'src/views/errors/NotFoundView';
 import ProductListView from 'src/views/product/ProductListView';
 import RegisterView from 'src/views/auth/RegisterView';
 import SettingsView from 'src/views/settings/SettingsView';
 import Dashboard from './pages/Dashboard';
 import Account from './pages/Account';
 import CustomerList from './pages/CustomerList';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
 
 const routes = [
   {
@@ -31,9 +33,9 @@ const routes = [
     path: '/',
     element: <MainLayout />,
     children: [
-      { path: 'login', element: <LoginView /> },
+      { path: 'login', element: <Login /> },
       { path: 'register', element: <RegisterView /> },
-      { path: '404', element: <NotFoundView /> },
+      { path: '404', element: <NotFound /> },
       { path: '/', element: <Navigate to="/app/dashboard" /> },
       { path: '*', element: <Navigate to="/404" /> }
     ]
