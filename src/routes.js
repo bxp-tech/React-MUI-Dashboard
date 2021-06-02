f58cd9da0e7188beb7c030dd12d89a57d3ef28ff
Pablo Lozano <juanpa92008@gmail.com>
2021-06-02 18:20:57 -0500

    Update Register & Settings

--- src/routes.js
+++ src/routes.js
@@ -2,20 +2,14 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import DashboardLayout from 'src/layouts/DashboardLayout';
 import MainLayout from 'src/layouts/MainLayout';
-// import CustomerListView from 'src/views/customer/CustomerListView';
-// import AccountView from 'src/views/account/AccountView';
-// import DashboardView from 'src/views/reports/DashboardView';
-// import LoginView from 'src/views/auth/LoginView';
-// import NotFoundView from 'src/views/errors/NotFoundView';
-// import ProductListView from 'src/views/product/ProductListView';
-import RegisterView from 'src/views/auth/RegisterView';
-import SettingsView from 'src/views/settings/SettingsView';
 import Dashboard from './pages/Dashboard';
 import Account from './pages/Account';
 import CustomerList from './pages/CustomerList';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
 import ProductList from './pages/ProductList';
+import Register from './pages/Register';
+import Settings from './pages/Settings';
 
 const routes = [
   {
@@ -26,8 +20,8 @@ const routes = [
       { path: 'customers', element: <CustomerList /> },
       { path: 'dashboard', element: <Dashboard /> },
       { path: 'products', element: <ProductList /> },
-      { path: 'settings', element: <SettingsView /> },
-      { path: '*', element: <Navigate to="/404" /> }
+      { path: 'settings', element: <Settings /> },
+      { path: '*', element: <Navigate to="/app/dashboard" /> }
     ]
   },
   {
@@ -35,9 +29,9 @@ const routes = [
     element: <MainLayout />,
     children: [
       { path: 'login', element: <Login /> },
-      { path: 'register', element: <RegisterView /> },
+      { path: 'register', element: <Register /> },
       { path: '404', element: <NotFound /> },
-      { path: '/', element: <Navigate to="/app/dashboard" /> },
+      { path: '/', element: <Navigate to="/login" /> },
       { path: '*', element: <Navigate to="/404" /> }
     ]
   }
